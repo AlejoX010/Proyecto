bc449077d51efd3cb0a895518bcdc75e87fda342
anuar <anuar@gmail.com>
2021-11-28 13:29:15 -0600

    Un pequeño arreglo

--- src/componentes/Cita.js
+++ src/componentes/Cita.js
@@ -1,16 +1,19 @@
 import React, {Fragment} from 'react';
-let total = '';
+// let total = '';
+// const Si = ({ cita, eliminarCita }) => (
+//  total = (parseInt(cita.interes)*parseInt(cita.cantidad))/100
+// )
 //Esto es para que muestre los objetos en un fichero aparte en el div 2 del app
 const Cita = ({ cita, eliminarCita }) => (
     <Fragment>
-    { total = (parseInt(cita.interes)*parseInt(cita.cantidad))/100}
+   
     <div className="cita">
         <p>Prestatario: <span>{cita.prestatario}</span></p>
         <p>Prestamista: <span>{cita.prestamista}</span></p>
         <p>Cantidad: <span>${cita.cantidad}</span></p>
         <p>Comicion: <span>{cita.interes}%</span></p>
-        <p>Ganancia: <span>${total}</span></p>
-        <p>Total: <span>${total + parseInt(cita.cantidad)}</span></p>
+        <p>Ganancia: <span>${(parseInt(cita.interes)*parseInt(cita.cantidad))/100}</span></p>
+        <p>Total: <span>${((parseInt(cita.interes)*parseInt(cita.cantidad))/100) + parseInt(cita.cantidad)}</span></p>
         <p>Fecha: <span>{cita.fecha}</span></p>
         <p>Hora: <span>{cita.hora}</span></p>
         <p>Descripcion: <span>{cita.descripcion}</span></p>
